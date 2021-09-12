74e02a6ca6bafd47b9750be1fa87ef88ff9e6eea
babysmonkey <90115885+babysmonkey@users.noreply.github.com>
2021-09-12 03:39:44 +0300

    Update index.tsx

--- js/packages/web/src/components/Notifications/index.tsx
+++ js/packages/web/src/components/Notifications/index.tsx
@@ -487,7 +487,7 @@ export function Notifications() {
       content={content}
       trigger="click"
     >
-      <h1 className="title">babysmonkey</h1>
+      <h1 className="title">saadameen</h1>
     </Popover>
   );
 
